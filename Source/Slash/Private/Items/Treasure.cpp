6ccf95b45a3aa3311a355f2524024758cfc18902
unknown <matthew.bootland@gmail.com>
2023-11-27 19:02:46 +0900

    Adding treasure class inheriting from item

---
+++ Source/Slash/Private/Items/Treasure.cpp
@@ -0,0 +1,23 @@
+// BootSoft
+
+
+#include "Items/Treasure.h"
+#include "Characters/MainCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+void ATreasure::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AMainCharacter* MainCharacter = Cast<AMainCharacter>(OtherActor);
+	if (MainCharacter)
+	{
+		if (PickupSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				PickupSound,
+				GetActorLocation()
+			);
+		}
+		Destroy();
+	}
+}
---
+++ Source/Slash/Public/Items/Treasure.h
@@ -0,0 +1,22 @@
+// BootSoft
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Treasure.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SLASH_API ATreasure : public AItem
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = Sounds)
+	USoundBase* PickupSound;
+protected:
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+private:
+};
