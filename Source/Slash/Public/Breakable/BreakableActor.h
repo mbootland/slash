70f2b5991dd80d8ba3f750bc0c7ff5eb67d34a49
unknown <matthew.bootland@gmail.com>
2023-11-27 17:39:21 +0900

    Breakable actor additions and break sounds. Plus additions to the hit interface to allow sounds to be added

---
+++ Source/Slash/Public/Breakable/BreakableActor.h
@@ -0,0 +1,28 @@
+// BootSoft
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/HitInterface.h"
+#include "BreakableActor.generated.h"
+
+class UGeometryCollectionComponent;
+
+UCLASS()
+class SLASH_API ABreakableActor : public AActor, public IHitInterface
+{
+	GENERATED_BODY()
+
+public:
+	ABreakableActor();
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UGeometryCollectionComponent* GeometryCollection;
+};
