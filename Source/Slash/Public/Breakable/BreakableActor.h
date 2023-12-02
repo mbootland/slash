bdb5c9bf69b90cf94dbcb07cb7680db6b1d6afeb
unknown <matthew.bootland@gmail.com>
2023-12-02 11:28:55 +0900

    adding treasure and fixing class error

--- Source/Slash/Public/Breakable/BreakableActor.h
+++ Source/Slash/Public/Breakable/BreakableActor.h
@@ -19,15 +19,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
-protected:
-	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Breakable Properties")
+	TArray<TSubclassOf<class ATreasure>> TreasureClasses;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UGeometryCollectionComponent* GeometryCollection;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	class UCapsuleComponent* Capsule;
-private:
-	UPROPERTY(EditAnywhere, Category = "Breakable Properties")
-	TSubclassOf<class ATreasure> TreasureClass;
+protected:
+	virtual void BeginPlay() override;
+	bool bBroken = false;
 };
--- Source/Slash/Public/Items/Treasure.h
+++ Source/Slash/Public/Items/Treasure.h
@@ -16,7 +16,9 @@ class SLASH_API ATreasure : public AItem
 public:
 	UPROPERTY(EditAnywhere, Category = Sounds)
 	USoundBase* PickupSound;
+
+	UPROPERTY(EditAnywhere, Category = "Treasure Properties")
+	int32 Gold;
 protected:
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
-private:
 };
