3dc4decc8d2a9ca62a0be3d6df394ebd81249e0d
unknown <matthew.bootland@gmail.com>
2023-12-03 16:55:08 +0900

    adding healthbar parent class

---
+++ Source/Slash/Private/HUD/HealthBar.cpp
@@ -0,0 +1,5 @@
+// BootSoft
+
+
+#include "HUD/HealthBar.h"
+
---
+++ Source/Slash/Public/HUD/HealthBar.h
@@ -0,0 +1,19 @@
+// BootSoft
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HealthBar.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class SLASH_API UHealthBar : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HealthBar;
+};
